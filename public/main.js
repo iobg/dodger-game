aa094e7991e92ddc3f4595753402230ba0cd8736
Banks Humphreys <iobg0320@gmail.com>
2016-10-06 12:12:56 -0500

    added diagonal movement and increased movement speed

--- public/main.js
+++ public/main.js
@@ -21,17 +21,33 @@ window.onload = function() {
   let gameLoop=()=>{
 
 	ctx.clearRect(0,0,500,500)
-	if (keyState[37] || keyState[65]){
-   		 player1.x -=1;
+  if(keyState[37] && keyState[40]){
+    player1.x-=2
+    player1.y+=2
+  }
+  else if(keyState[39] && keyState[40]){
+    player1.x+=2
+    player1.y+=2
+  }
+  else if(keyState[37] && keyState[38]){
+    player1.x-=2
+    player1.y-=2
+  }
+  else if(keyState[39] && keyState[38]){
+    player1.x+=2
+    player1.y-=2
+  }
+	else if (keyState[37]){
+   		 player1.x -=2;
     	}
-  else if (keyState[39] || keyState[65]){
-   		 player1.x +=1;
+  else if (keyState[39]){
+   		 player1.x +=2;
     	}
-  else if (keyState[38] || keyState[65]){
-   		 player1.y -=1;
+  else if (keyState[38]){
+   		 player1.y -=2;
     	}
-  else if (keyState[40] || keyState[65]){
-   		 player1.y +=1;
+  else if (keyState[40]){
+   		 player1.y +=2;
     	}
     ctx.drawImage(player1.image,player1.x,player1.y)
     setTimeout(gameLoop,10)
@@ -39,27 +55,4 @@ window.onload = function() {
 
 gameLoop()
 };
-
-
-
-
-
-
-
-
-// const movePlayer=(player)=>{
-// 	ctx.clearRect(0,0,500,500)
-// 	if(event.key==='ArrowLeft'){
-// 		ctx.drawImage(player.image,player.x-=10,player.y)
-// 		}
-// 		if(event.key==='ArrowUp'){
-// 		ctx.drawImage(player.image,player.x,player.y-=10)
-// 		}
-// 		if(event.key==='ArrowDown'){
-// 		ctx.drawImage(player.image,player.x,player.y+=10)
-// 		}
-// 		if(event.key==='ArrowRight'){
-// 		ctx.drawImage(player.image,player.x+=10,player.y)
-// 		}
-// }
 
