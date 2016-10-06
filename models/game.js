d2116486ee5fe2fbd3f4130d65e2fd06ed6e55c3
Zachary George <zacharygeorge@Zacharys-MacBook-Pro.local>
2016-10-06 14:35:18 -0500

    added game model

---
+++ models/game.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('Game', {
+  obsticales: [Object],
+  player1: {
+    x: Number,
+    y: Number,
+    name: String
+  },
+  player2: {
+    x: Number,
+    y: Number,
+    name: String
+  }
+})
